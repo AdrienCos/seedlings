c074429dbee3142d0bc453ab35a10b06999cd5d0
AdrienCos <ad.cosson@gmail.com>
2023-07-31 18:58:44 +0200

    Migrate to copier

--- python/{{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Top-level package for {{ cookiecutter.project_name }}.
-
-.. include:: ../../README.md
-"""
-
-__author__ = "{{ cookiecutter.full_name }}"
-__email__ = "{{ cookiecutter.email }}"
-__docformat__ = "google"
--- python/{{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/example_module/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Example to show the standard way of creating and importing sub-packages."""
-
-from {{ cookiecutter.project_slug }}.example_module.example_class import ExampleClass
