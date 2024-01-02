dfdf8621f058e9a31a4083250834ae662c47f5c3
Adrien Cosson <adrien.cosson@elmy.fr>
2024-01-02 15:57:52 +0100

    feat: setup: add a docstring to explain the existence of the setup.py

--- python/setup.py
+++ python/setup.py
@@ -1,3 +1,11 @@
+"""This file is only here to allow older version of setuptools to install this
+package. In the future, when we are confident that all these setuptools
+versions are gone from the developers and production machines, this file
+can be removed.
+
+See: https://setuptools.pypa.io/en/latest/userguide/declarative_config.html
+"""
+
 from setuptools import setup
 
 setup()
